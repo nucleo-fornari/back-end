0e5a1f0ce14b1304a7834eb79011ff80fef80dcd
Felipe-Baamonde <felipe.baamonde@sptech.school>
2024-10-30 18:52:22 -0300

    FEAT: Exportar usuários para csv

---
+++ api/fornari.nucleo/src/main/java/fornari/nucleo/service/ExportService.java
@@ -0,0 +1,49 @@
+package fornari.nucleo.service;
+
+import fornari.nucleo.entity.Usuario;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExportService {
+    public static void exportar(List<Usuario> usuarios) {
+        try (
+                OutputStream file = new FileOutputStream("usuarios.csv");
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(file));
+        ) {
+            writer.write("ID;Nome;CPF;Email;Funcao\n");
+            String pattern = "%d;%s;%s;%s;%s\n";
+
+            usuarios.stream()
+                    .map(usuario -> pattern.formatted(
+                            usuario.getId(),
+                            usuario.getNome(),
+                            usuario.getCpf(),
+                            usuario.getEmail(),
+                            usuario.getFuncao()))
+                    .forEach(linha -> {
+                        try {
+                            writer.write(linha);
+                        } catch (IOException e) {
+                            System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+                        }
+                    });
+
+        } catch (FileNotFoundException erro) {
+            System.out.println("Erro ao encontrar o arquivo");
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo");
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1, "Felipe", "12322465543", "felipe.teste@gmail.com", null, null, "Professor", null));
+        usuarios.add(new Usuario(2, "Ana", "98765432100", "ana.exemplo@gmail.com", null, null, "Professor", null));
+        usuarios.add(new Usuario(3, "Carlos", "45678912301", "carlos.exemplo@gmail.com", null, null, "Responsavel", null));
+        usuarios.add(new Usuario(4, "Julia", "32145698710", "julia.exemplo@gmail.com", null, null, "Secretario", null));
+        usuarios.add(new Usuario(5, "Marcos", "65412398710", "marcos.exemplo@gmail.com", null, null, "Responsavel", null));
+        exportar(usuarios);
+    }
+}
